3e3fcf55a40c2feb7467b2cdac84b21c5286325a
Aarush <kraarush709@gmail.com>
2025-03-11 19:55:51 +0530

    tested and added getAppliedJobs route in application.controller and modified the application.route for this particular route

--- Backend/controllers/application.controller.js
+++ Backend/controllers/application.controller.js
@@ -1,12 +1,13 @@
 import Application from "../models/application.model.js";
 import Job from "../models/job.model.js";
 
-export const applyJob = async (req, res) => {
+
+export const applyJob = async (req, res) => {               // student
     try {
         const userId = req.id;
         const jobId = req.params.id;
 
-        if(!jobId){
+        if (!jobId) {
             return res.status(404).json({
                 message: "Job Id not found",
                 success: false,
@@ -23,8 +24,8 @@ export const applyJob = async (req, res) => {
         }
 
         // checking if the user already applied for the job
-        const isAlreadyApplied = await Application.findOne({job: jobId, applicant: userId});
-        if(isAlreadyApplied){
+        const isAlreadyApplied = await Application.findOne({ job: jobId, applicant: userId });
+        if (isAlreadyApplied) {
             return res.status(409).json({
                 message: "Already applied for job",
                 success: false,
@@ -48,6 +49,63 @@ export const applyJob = async (req, res) => {
     } catch (error) {
         return res.status(500).json({
             message: "Internal server error applying for job, " + error.message,
+            success: false,
+        });
+    }
+}
+
+// can be more secured by not sending the populated results to the user with sensitve data -- will work on that later
+export const getAppliedJobs = async (req, res) => {             // student
+    try {
+        const userId = req.id;
+        
+        const jobs = await Application.find({applicant: userId}).populate({
+            path:'job',
+            populate: {
+                path: 'company',
+            }
+        }).sort({createdAt: -1});
+
+        if(jobs.length === 0){
+            return res.status(404).json({
+                message: "No job applications found",
+                success: false,
+            });
+        }
+
+        return res.status(200).json({
+            jobs,
+            success: true,
+        });
+
+    }
+    catch (error) {
+        return res.status(500).json({
+            message: "Internal server error getting applied jobs, " + error.message,
+            success: false,
+        });
+    }
+};
+
+
+export const getApplicants = async (req, res) => {              // admin
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error getting applicants, " + error.message,
+            success: false,
+        });
+    }
+}
+
+
+export const updateStatus = async (req, res) => {               // admin
+    try {
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error updating status of applicant, " + error.message,
             success: false,
         });
     }
--- Backend/routes/application.route.js
+++ Backend/routes/application.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authmiddleware.js';
 import authorizeRole from '../middlewares/authorizeRole.js';
-import { applyJob } from '../controllers/application.controller.js';
+import { applyJob, getAppliedJobs } from '../controllers/application.controller.js';
 
 const router = express.Router();
 
 router.route('/apply/:id').get(authMiddleware, authorizeRole("student"), applyJob);
+router.route('/get').get(authMiddleware, authorizeRole("student"), getAppliedJobs);
+
 
 export default router;
